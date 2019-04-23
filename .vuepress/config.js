170b38e487486510fb14faaaaaa6fdd7fa35c24f
sombriks <sombriks@gmail.com>
2019-04-23 16:59:00 -0300

    adding last updated

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -3,4 +3,11 @@ module.exports = {
   description: "Various SDK implementations to consume the Panamah API",
   base: process.env.NODE_ENV == "development" ? "/" : "/panamah-sdk/",
   dest: "docs",
+  themeConfig: {
+    lastUpdated: true,
+    logo: "/assets/img/panamah-sdk.png",
+    nav: [
+      { text: "Github", link: "https://github.com/casamagalhaes/panamah-sdk" },
+    ],
+  },
 };
--- README.md
+++ README.md
@@ -1,15 +1,18 @@
 ---
 home: true
 heroImage: /assets/img/panamah-sdk.png
+footer: MIT Licensed | © Casas Magalhães
 ---
 # panamah-sdk
 
 The panamah standard developer kit
 
-## How to use
+## How to use the SDK
 
 The SDK is available in various languages and counts with a simple and loosely
 coupled API.
+
+See [overview](OVERVIEW.md) for more details.
 
 ## Where are the code
 
