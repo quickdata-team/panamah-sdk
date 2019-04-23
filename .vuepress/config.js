8059fd5acfdf75af24ee4d6be5f0376a0bba9cd8
sombriks <sombriks@gmail.com>
2019-04-23 14:36:24 -0300

    see if base is being calculated correctly

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,4 +1,6 @@
 module.exports = {
-    base:"/panamah-sdk/",
-    dest:"docs"
-}
+  title: "Casas Magalhães - Panamah Standard Developer Kit",
+  description: "Various SDK implementations to consume the Panamah API",
+  base: process.env.NODE_ENV == "development" ? "/" : "/panamah-sdk/",
+  dest: "docs",
+};
